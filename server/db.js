48f119b3b0739f4ea6129acd84b555bae87d4ebe
tt-sanchez06 <tomase.sanchezl@gmail.com>
2025-11-17 20:54:32 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
+  user: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT || 33415,
